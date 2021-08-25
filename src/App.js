f99edc1acf061dbd5995ff08f6bd2e0a476a0c88
SamanAtashi <milady00126@gmail.com>
2021-08-25 06:36:16 +0300

    Add react router to pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 import './App.css';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
 
 const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
+  <Switch>
+    <Route path="/calculator">
+      <div className="App">
+        <Calculator />
+      </div>
+    </Route>
+    <Route path="/quote">
+      <Quote />
+    </Route>
+    <Route path="/">
+      <Home />
+    </Route>
+  </Switch>
 );
 
 export default App;
---
+++ src/pages/Quote.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Quote = () => <p>inside Quote</p>;
+
+export default Quote;
